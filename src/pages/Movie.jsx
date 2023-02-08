c8d87bdcdfe67d3f87ab8984ad30611c6bab2529
Nzy <nidal.chowdhury1@gmail.com>
2023-02-08 23:51:23 +1100

    init

--- src/pages/Movie.jsx
+++ src/pages/Movie.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import "./Movie.css";
+import PlayCircleOutlineIcon from '@mui/icons-material/PlayCircleOutline';
 
 export default function Movie({ currentMovie }) {
   const base_url = "https://image.tmdb.org/t/p/original/";
@@ -14,15 +15,26 @@ export default function Movie({ currentMovie }) {
         alt=""
       />
       <div className="movie__info">
-        <h1>{currentMovie.title || currentMovie.original_name || currentMovie.Title}</h1>
-        <p className="movie_date">Movie Date: {currentMovie.release_date || currentMovie.Year}</p>
+        <h1>
+          {currentMovie.title ||
+            currentMovie.original_name ||
+            currentMovie.Title}
+        </h1>
+        <p className="movie_date">
+          Movie Date: {currentMovie.release_date || currentMovie.Year}
+        </p>
         <p className="movie_overview">
           Movie Overview: <br />
-          {currentMovie.overview || 'n/a'}
+          {currentMovie.overview || "n/a"}
           <br />
           <br />
           <br />
-          Movie Popularity: {currentMovie.popularity || `n/a`}
+          <div className="btm">
+            <>Movie Popularity: {currentMovie.popularity || `n/a`}</>
+            <button className="play_btn">
+              <PlayCircleOutlineIcon />
+            </button>
+          </div>
         </p>
       </div>
     </div>
